d7bc0a5d273ab9b2315c1bc1520c25f8cef4792c
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-10 16:04:17 -0300

    Feito o layout das To-Do list

--- tasks/src/components/Todos.vue
+++ tasks/src/components/Todos.vue
@@ -1,28 +1,24 @@
 <template>
-    <div>
-        <div v-bind:key="todo.id" v-for="todo in todos">
-            
-            <!-- Enviando o  loop para o todoitem.vue template-->
-            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"></TodoItem>
-        </div>
-    
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <!-- Enviando o  loop para o todoitem.vue template-->
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"></TodoItem>
     </div>
+  </div>
 </template>
 
 <script>
-
-import TodoItem from './toDoItem.vue'
+import TodoItem from "./toDoItem.vue";
 export default {
-    name: "Todos",
-    components: {
-        TodoItem
-    },
-    // props é para pgar um valor que foi passado no v-bind quando chamou o template
-    props:["todos"]
-}
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  // props é para pgar um valor que foi passado no v-bind quando chamou o template
+  props: ["todos"]
+};
 </script>
 
 
 <style scoped>
-    
 </style>
--- tasks/src/components/toDoItem.vue
+++ tasks/src/components/toDoItem.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
     <p>
-      <input type="checkbox" v-on:change="markComplete">
-      {{todo.title}}
+      <label class="container-checkbox">
+        {{todo.title}}
+        <input type="checkbox" :checked="todo.completed" v-on:change="markComplete">
+        <span class="checktrue"></span>
+      </label>
+
       <button @click="$emit('del-todo', todo.id)" class="del">X</button>
     </p>
   </div>
@@ -40,5 +44,66 @@ export default {
   border-radius: 50%;
   cursor: pointer;
   float: right;
+}
+
+.container-checkbox {
+  /* display: block; */
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.container-checkbox input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+.checktrue {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  border: 1px #0b3c5d solid;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.container-checkbox:hover input ~ .checktrue {
+  background-color: #ccc;
+}
+
+.container-checkbox input:checked ~ .checktrue {
+  background-color: #d9b310;
+}
+
+.checktrue:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.container-checkbox input:checked ~ .checktrue:after {
+  display: block;
+}
+
+.container-checkbox .checktrue:after {
+  left: 7px;
+  top: 3.5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
 </style>
