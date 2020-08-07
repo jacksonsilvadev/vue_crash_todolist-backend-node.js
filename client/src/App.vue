e83ceac779d55ad10ee09926091e748f04e7229d
Jackson Silva <jackson.rodrigues9@hotmail.com>
2020-08-06 23:46:34 -0300

    Added a script url

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <script data-ad-client="ca-pub-3502094030340133" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <div id="nav">
       <Header></Header>
       <!-- <router-link to="/">Home</router-link> |
